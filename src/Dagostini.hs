6521e4751dcc6df46ed8c7d9734c5df7d925a7cc
Chris Pollard <cspollard@gmail.com>
2017-01-17 20:41:19 +0000

    seems to be working!

--- src/Dagostini.hs
+++ src/Dagostini.hs
@@ -1,114 +1,81 @@
-{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DataKinds                 #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE RankNTypes                #-}
+{-# LANGUAGE TypeFamilies              #-}
+{-# LANGUAGE TypeOperators             #-}
 
 module Dagostini where
 
-import           Control.Monad.Primitive
-import           Data.Foldable
-import           Data.Monoid
-import           Data.Traversable
-import           Data.Vector.Fixed.Cont        (Arity (..), ContVec (..),
-                                                ToPeano (..))
-import qualified Data.Vector.Fixed.Cont        as V
-import           System.Random.MWC.Probability hiding (dirichlet)
-
--- | The dirichlet distribution.
-dirichlet :: (Traversable f, PrimMonad m) => f Double -> Prob m (f Double)
-dirichlet as = do
-  zs <- mapM (`gamma` 1) as
-  return $ let (s, zs') = mapAccumL (\x z -> (x+z, z/s)) 0 zs in zs'
-{-# INLINABLE dirichlet #-}
+import qualified Data.Vector.Fixed.Cont as V
+import           Likelihood
+import qualified List.Transformer       as LT
+import           Matrix
 
 
-type Vec = ContVec
 
-instance (Show a, Arity n) => Show (ContVec n a) where
-  show = show . toList
+postLLH :: (Integral b, Floating a, Arity neffect, Arity ncause)
+  => Vec neffect b
+  -> Vec neffect a
+  -> Mat ncause (S neffect) a
+  -> a
+  -> (Vec ncause a -> a)
+  -> Vec ncause a -> a
+postLLH dat bkgs smear lumi logPrior sigmas =
+  conditionalLogProb + logPrior sigmas
+  where
+    -- NB: the first row of the migration matrix should be the inefficiency of
+    --     each cause bin.
+    -- xs: the prediction based on the given cross sections
+    xs = fmap (*lumi) $ (+) <$> tailV (multMV smear sigmas) <*> bkgs
 
--- rows are innermost
-type Mat n m a = Vec m (Vec n a)
-
-transpose :: (Arity n, Arity m) => Mat n m a -> Mat m n a
-transpose = sequenceA
-
-inner :: (Monoid c, Foldable t, Applicative t)
-      => (a -> b -> c) -> t a -> t b -> c
-inner f v v' = fold $ f <$> v <*> v'
-
-dot :: (Num a, Foldable t, Applicative t)
-    => t a -> t a -> a
-dot v v' = getSum $ inner (\x y -> Sum $ x * y) v v'
-
-multMM :: (Traversable t1, Traversable t, Applicative t1, Applicative f, Num b)
-       => t (t1 b) -> t1 (f b) -> f (t b)
-multMM m m' = outer dot m $ sequenceA m'
-
-multMV :: (Traversable t, Foldable f, Applicative f, Num b)
-       => t (f b) -> f b -> t b
-multMV = traverse dot
-
-multVM :: (Traversable t1, Traversable t, Applicative t1, Applicative t, Num b)
-       => t1 b -> t1 (t b) -> t b
-multVM v m = traverse dot (sequenceA m) v
-
--- arbitrary cartesian product
-infix 5 ><
-(><) :: (Traversable t, Functor f) => t a -> f b -> f (t (a, b))
-(><) = outer (,)
-
-outer :: (Traversable t, Functor f)
-      => (a1 -> a -> b) -> t a1 -> f a -> f (t b)
-outer f v v' = traverse f v <$> v'
+    -- the log conditional probability of the data bins given the sigmas
+    conditionalLogProb = sum $ logPoissonP <$> dat <*> xs
 
 
 -- test case:
 -- 3 data bins
 -- 2 cause bins
--- 1 trash bin
 
-myData :: Vec (ToPeano 3) Int
-myData = V.fromList' [1, 2, 3]
+type NE = ToPeano 3
+type NC = ToPeano 2
 
-myLambdas :: Mat (ToPeano 2) (ToPeano 4) Double
-myLambdas = V.fromList'
-  [ V.fromList' [ 0.25, 0.25 ]
-  , V.fromList' [ 0.25, 0.50 ]
-  , V.fromList' [ 0.00, 0.25 ]
-  , V.fromList' [ 0.50, 0.00 ]
+-- TODO
+-- odd numbers are broken for some reason
+test :: Int -> IO ()
+test n = LT.runListT $ LT.liftIO . print =<< c
+
+  where
+    f :: Floating a => Vec NC a -> a
+    f = iterate (postLLH myData myBkgs mySmears 1.0) myLogPrior !! n
+    c =
+      LT.take 100
+        $ runLLH (metropolis 1.0) f (V.fromList' [1, 1])
+          =<< LT.liftIO createSystemRandom
+
+myData :: Vec NE Int
+myData = V.fromList' [1, 2, 1]
+
+myBkgs :: Fractional a => Vec NE a
+myBkgs = V.fromList' [0.0, 0.0, 0.0]
+
+mySmears :: Fractional a => Mat NC (S NE) a
+mySmears = V.fromList'
+  [ V.fromList' [0.0, 0.0]
+  , V.fromList' [0.5, 0.0]
+  , V.fromList' [0.0, 1.0]
+  , V.fromList' [0.5, 0.0]
   ]
 
-myPrior :: Vec (ToPeano 2) Double
-myPrior = V.fromList' [1, 1000]
-
-myThetas :: Mat (ToPeano 4) (ToPeano 2) Double
--- TODO
--- this line could be cleaned I'm sure
-myThetas = (\x -> (/) <$> x <*> denom) <$> numer
-  where
-    ljis = transpose myLambdas
-    pis = myPrior
-    -- TODO
-    -- this line could be cleaned I'm sure
-    numer = (\x y -> fmap (*y) x) <$> ljis <*> pis
-    denom = multVM pis ljis
-
-myEfficiency :: Vec (ToPeano 2) Double
-myEfficiency = (-) <$> pure 1 <*> V.head myLambdas
-
-myMeas :: Vec (ToPeano 2) Double
-myMeas = (/) <$> multMV smear dat <*> myEfficiency
-  where
-    smear = (transpose . V.tail . transpose) myThetas
-    dat = fromIntegral <$> myData
-
-
+myLogPrior :: Num a => Vec NC a -> a
+myLogPrior = const 0
 
 {-
 
 -- Ci: cause bin i
--- xi: events in cause bin i (what we are measuring)
--- xij: events in cause bin i and effect bin j
+-- si: events in cause bin i due to process in question (what we are measuring)
+-- sij: events in cause bin i and effect bin j
 -- Ej: effect bin j
--- yj: events in effect bin j (what we observe)
+-- xj: events in effect bin j (what we observe)
 
 -- P(Ej|Ci):
 --   probability that an event lands in effect bin j given an event in cause
@@ -125,8 +92,8 @@ myMeas = (/) <$> multMV smear dat <*> myEfficiency
 --   where
 --     P(Ej) = sum_i P(Ej|Ci) P(Ci)
 
--- so xij = P(Ci|Ej) yj
--- xi = (sum_j P(Ci|Ej) yj) / (sum_j P(Ej|Ci))
+-- so sij = P(Ci|Ej) xj
+-- si = (sum_j P(Ci|Ej) xj) / (sum_j P(Ej|Ci))
 --   where sum_j P(Ej|Ci) is the observation efficiency of cause bin i
 
 -- we observe a spectrum xj (integers) in n effect bins.
@@ -141,5 +108,7 @@ myMeas = (/) <$> multMV smear dat <*> myEfficiency
 
 -- we want P(si|xj) = P(xj|si) P(si) / P(xj)
 --   - where P(xj) = bj + sum_i P(Ej|Ci) P(si)
+
+-- P(sigma)
 
 -}
