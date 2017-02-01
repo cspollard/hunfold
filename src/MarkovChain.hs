f57b76618affb04c8df5226f16cd4d2aa8a2d8d2
Chris Pollard <cspollard@gmail.com>
2017-02-01 10:09:14 +0000

    back to declarative...

--- src/MarkovChain.hs
+++ src/MarkovChain.hs
@@ -4,33 +4,20 @@
 module MarkovChain
   ( module X
   , runMC
-  , weightedProposal
   ) where
 
-import           List.Transformer              as X (ListT (..), Step (..),
-                                                     liftIO, runListT)
-import           Probability
-import           System.Random.MWC             as X (Gen, asGenIO,
-                                                     withSystemRandom)
-import           System.Random.MWC.Probability as X (Prob, Variate, sample,
-                                                     samples, uniform)
+import           List.Transformer                 as X (ListT (..), Step (..),
+                                                        liftIO, runListT)
+import           Numeric.MCMC
+
+import           Control.Monad.Trans.State.Strict
+import           System.Random.MWC                as X (Gen, asGenIO,
+                                                        withSystemRandom)
+import           System.Random.MWC.Probability    as X (Prob, Variate, sample,
+                                                        samples, uniform)
 
 runMC :: PrimMonad m
-      => (a -> Prob m a) -> a -> Gen (PrimState m) -> ListT m a
+      => Transition m a -> a -> Gen (PrimState m) -> ListT m a
 runMC t s g = ListT $ do
-  s' <- sample (t s) g
+  s' <- sample (execStateT t s) g
   return . Cons s' $ runMC t s' g
-
-
--- a proposal weighted by a log likelihood function
-weightedProposal :: (Floating b, Variate b, PrimMonad m, RealFloat b)
-                 => (a -> Prob m a) -> (a -> b) -> T a b -> Prob m (T a b)
-weightedProposal t logLH (T x y) =
-  do
-    prop <- t x
-    let score = logLH prop
-    -- TODO
-    -- what happens with NaNs?
-    let prob = exp . min 0 $ score - y
-    accept <- if isNaN prob then return False else bernoulli prob
-    return $ if accept then T prop score else T x y
