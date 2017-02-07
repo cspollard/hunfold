34cf42490ea0641af867318f1798c0c7231a228c
Chris Pollard <cspollard@gmail.com>
2017-02-07 10:08:49 +0000

    moving away from hmatrix.

--- src/InMatrix.hs
+++ src/InMatrix.hs
@@ -1,60 +1,46 @@
 {-# LANGUAGE FlexibleContexts          #-}
 {-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE RankNTypes                #-}
 {-# LANGUAGE TupleSections             #-}
-{-# LANGUAGE TypeFamilies             #-}
-{-# LANGUAGE RankNTypes             #-}
+{-# LANGUAGE TypeFamilies              #-}
 
 module InMatrix
   ( module X
   , toMatrix, inM, inM2
-  , toVector, inV
-  , invM, eigM, cholM
+  , invM, cholM
   ) where
 
-import           Control.Lens          hiding (Indexable)
-import           Numeric.LinearAlgebra as X
+import           Control.Lens hiding (Indexable)
+import           Data.Matrix  as X
 
--- TODO
--- these could probably be rewritten in terms of the Vector equivalents below.
-toMatrix :: (Container Vector b, Num b, Traversable t, Traversable t1)
+-- NB
+-- Matrix is indexed starting at 1
+toMatrix :: (Traversable t, Traversable t1)
          => t (t1 b) -> (Matrix b -> t (t1 b), Matrix b)
 toMatrix xs =
-  let back = iover traversed const
-      bb = iover traversed (\i x -> (i,) <$> back x) xs
-      f m = over (traverse.traverse) (atIndex m) bb
+  let back = iover traversed (const . (+1))
+      bb = iover traversed (\i x -> (i+1,) <$> back x) xs
+      f m = over (traverse.traverse) (m !) bb
       ls = toListOf (traverse . to (toListOf traverse)) xs
   in (f, fromLists ls)
 
-inM :: (Container Vector b, Num b, Traversable t, Traversable t1) => (Matrix b -> Matrix b) -> t (t1 b) -> t (t1 b)
+inM
+  :: (Functor f, Traversable t, Traversable t1)
+  => (Matrix b -> f (Matrix b)) -> t (t1 b) -> f (t (t1 b))
 inM f xs =
   let (b, m) = toMatrix xs
-  in b $ f m
+  in b <$> f m
 
-inM2 :: (Container Vector a, Traversable t, Traversable t1, Num a) => (Matrix a -> Matrix a -> Matrix a) -> t (t1 a) -> t (t1 a) -> t (t1 a)
+inM2
+  :: (Functor f, Traversable t, Traversable t1)
+  => (Matrix a -> Matrix a -> f (Matrix a)) -> t (t1 a) -> t (t1 a) -> f (t (t1 a))
 inM2 f xs ys =
   let (bx, mx) = toMatrix xs
       (_, my) = toMatrix ys
-  in bx $ f mx my
+  in bx <$> f mx my
 
-invM :: (Field b, Traversable t1, Traversable t) => t (t1 b) -> t (t1 b)
-invM = inM inv
+invM :: (Traversable t1, Traversable t, Fractional b, Eq b) => t (t1 b) -> Either String (t (t1 b))
+invM = inM inverse
 
-cholM :: (Field b, Traversable t1, Traversable t) => t (t1 b) -> t (t1 b)
-cholM = inM (chol . sym)
-
-eigM :: (Traversable t1, Traversable t) => t (t1 (Complex Double)) -> t (t1 (Complex Double))
-eigM = inM (snd . eig)
-
-
-toVector :: (Container Vector b, Traversable t)
-         => t b -> (Vector b -> t b, Vector b)
-toVector xs =
-  let back = iover traversed const xs
-      f v = over traverse (atIndex v) back
-      vs = toListOf traverse xs
-  in (f, fromList vs)
-
-inV :: (Container Vector b, Traversable t) => (Vector b -> Vector b) -> t b -> t b
-inV f xs =
-  let (b, v) = toVector xs
-  in b $ f v
+cholM :: (Traversable t1, Traversable t, Floating b) => t (t1 b) -> t (t1 b)
+cholM = runIdentity . inM (Identity . cholDecomp)
