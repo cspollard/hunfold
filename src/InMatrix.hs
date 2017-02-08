8d0561e2b28b481e8313199294de3f0217cf71d5
Chris Pollard <cspollard@gmail.com>
2017-02-08 08:56:11 +0000

    some rearrangement

--- src/InMatrix.hs
+++ src/InMatrix.hs
@@ -13,30 +13,30 @@ module InMatrix
 import           Control.Lens hiding (Indexable)
 import           Data.Matrix  as X
 
--- NB
+-- NB!!
 -- Matrix is indexed starting at 1
 toMatrix :: (Traversable t, Traversable t1)
-         => t (t1 b) -> (Matrix b -> t (t1 b), Matrix b)
+         => t (t1 b) -> (Matrix b, Matrix b -> t (t1 b))
 toMatrix xs =
   let back = iover traversed (const . (+1))
       bb = iover traversed (\i x -> (i+1,) <$> back x) xs
       f m = over (traverse.traverse) (m !) bb
       ls = toListOf (traverse . to (toListOf traverse)) xs
-  in (f, fromLists ls)
+  in (fromLists ls, f)
 
 inM
   :: (Functor f, Traversable t, Traversable t1)
   => (Matrix b -> f (Matrix b)) -> t (t1 b) -> f (t (t1 b))
 inM f xs =
-  let (b, m) = toMatrix xs
+  let (m, b) = toMatrix xs
   in b <$> f m
 
 inM2
   :: (Functor f, Traversable t, Traversable t1)
   => (Matrix a -> Matrix a -> f (Matrix a)) -> t (t1 a) -> t (t1 a) -> f (t (t1 a))
 inM2 f xs ys =
-  let (bx, mx) = toMatrix xs
-      (_, my) = toMatrix ys
+  let (mx, bx) = toMatrix xs
+      (my, _) = toMatrix ys
   in bx <$> f mx my
 
 invM :: (Traversable t1, Traversable t, Fractional b, Eq b) => t (t1 b) -> Either String (t (t1 b))
